1f3edd72cf0dc74c61ca388327976366042759c9
Geoff St. Pierre <serundeputy@gmail.com>
2020-02-20 08:05:37 -0500

    Adding get-tweets simplified return; adding abraham/twitteroauth.

--- get-tweets.php
+++ get-tweets.php
@@ -4,55 +4,54 @@
  * Aggregeate tweets from @serundeputy for display on serundeputy.io.
  */
 
-require_once "/vendor/autoload.php";
+require_once __DIR__ . "/vendor/autoload.php";
 
 use Abraham\TwitterOAuth\TwitterOAuth;
 
-/**
- * Implements hook_block_info().
- */
-function serundeputy_tweets_block_info() {
-  $blocks['serundeputy_tweets_block'] = array(
-    'info' => t('Serundeputy Tweets'),
-    'description' => t('Get tweets from twitter API.'),
-  );
 
-  return $blocks;
-}
-
-/**
- * Implements hook_block_view().
- *
- * @see _serundeputy_tweets_get_tweets()
- */
-function serundeputy_tweets_block_view($delta = '', $settings = array()) {
-  $block = array();
-
-  // Block content.
-  $content = _serundeputy_tweets_get_tweets();
-
-  switch ($delta) {
-    case 'serundeputy_tweets_block':
-      $block['subject'] = NULL;
-      $block['content'] = $content;
-      return $block;
-  }
-}
-
+// Get the tweets.
+$tweets = get_tweets();
+print_r($tweets['simplified_statuses']);
 /**
  * Get tweets.
  */
-function _serundeputy_tweets_get_tweets() {
-  // @codingStandardsIgnoreStart
-  global $settings;
-  // @codingStandardsIgnoreEnd
-
+function get_tweets() {
   $consumer_key = getenv('TWITTER_CONSUMER_KEY');
   $consumer_secret = getenv('TWITTER_CONSUMER_SECRET');
   $access_token = getenv('TWITTER_ACCESS_TOKEN');
   $access_token_secret = getenv('TWITTER_ACCESS_TOKEN_SECRET');
 
-  $connection = new TwitterOAuth($consumer_key, $consumer_secret, $access_token, $access_token_secret);
-  $statuses = $connection->get(
-    "search/tweets", ["q" => "serundeputy","count" => 4]
+  $connection = new TwitterOAuth(
+		$consumer_key,
+		$consumer_secret,
+    $access_token,
+    $access_token_secret
   );
+  $statuses = $connection->get(
+    "search/tweets", ["q" => "backdropcms"]
+  );
+  
+  $simplified_statuses = [];
+  foreach ($statuses as $status) {
+    foreach ($status as $data) {
+      if (isset($data->user->screen_name)) {
+        if (isset($data->retweeted_status)) {
+			    $url = $data->retweeted_status->entities->urls[0]->expanded_url;	
+				}
+				else {
+          $url = $data->entities->urls[0]->expanded_url;
+        }
+        $simplified_statuses[] = [
+          'author' => '@' . $data->user->screen_name,
+          'url' => $url,
+          'text' => $data->text,
+        ];
+      }
+    }
+  }
+
+  return [
+    'statuses' => $statuses,
+    'simplified_statuses' => $simplified_statuses,
+  ];
+}
