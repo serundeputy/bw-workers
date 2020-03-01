27303036510fb8d57e23be47bff8caec36c636ed
Geoff St. Pierre <serundeputy@gmail.com>
2020-03-01 11:39:21 -0500

    Catch tests up to new get-releases for any org structure.

--- tests/BwwTests.php
+++ tests/BwwTests.php
@@ -11,7 +11,9 @@ class BwwTests extends TestCase {
    * Test drush ctl command.
    */
   public function testGetNewReleasesSinceDate() {
-    $output = shell_exec('php get-releases.php 2020-02-26T01:08:20Z TRUE');
+    $output = shell_exec(
+      'php get-releases.php 2020-02-26T01:08:20Z backdrop-contrib TRUE'
+    );
     $this->assertStringContainsString(
       'This is a test run and only the first 30 results will be used',
       $output
